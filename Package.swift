b984394846de412e20469a487f323d4c4f3fd396
xcode73 <xcode.uln@gmail.com>
2024-02-23 16:05:15 +0400

    swift-tools-version:5.9

--- Package.swift
+++ Package.swift
@@ -1,27 +1,28 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "blog-module",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v10_15)
     ],
     products: [
-        .library(name: "BlogModule", targets: ["BlogModule"]),
+        .library(name: "BlogModule", targets: ["BlogModule"])
     ],
     dependencies: [
-        .package(url: "https://github.com/xcode73/feather-core", .branch("test-dev")),
-        .package(url: "https://github.com/xcode73/blog-objects", .branch("test-dev")),
-        .package(url: "https://github.com/xcode73/web-module", .branch("test-dev")),
+        .package(url: "https://github.com/xcode73/feather-core", branch: "test-dev"),
+        .package(url: "https://github.com/xcode73/blog-objects", branch: "test-dev"),
+        .package(url: "https://github.com/xcode73/web-module", branch: "test-dev")
     ],
     targets: [
         .target(name: "BlogModule", dependencies: [
             .product(name: "Feather", package: "feather-core"),
             .product(name: "BlogObjects", package: "blog-objects"),
-            .product(name: "WebModule", package: "web-module"),
+            .product(name: "WebModule", package: "web-module")
         ],
         resources: [
-            .copy("Bundle"),
+            .copy("Bundle")
         ]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
