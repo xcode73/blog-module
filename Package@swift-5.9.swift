e30161be86d814a0121cbd887faae5f3d400461f
Nikolai Eremenko <nikolai.dev@icloud.com>
2024-06-07 12:27:08 +0400

    build: update dependencies

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 import PackageDescription
 import Foundation
 
@@ -19,7 +19,8 @@ let package = Package(
             name: "BlogApi",
             dependencies: [
                 .product(name: "FeatherCoreApi", package: "feather-core"),
-            ]
+            ],
+            swiftSettings: [.enableExperimentalFeature("StrictConcurrency=complete")]
         ),
         .target(
             name: "BlogModule",
@@ -29,7 +30,8 @@ let package = Package(
             ],
             resources: [
                 .copy("Bundle"),
-            ]
+            ],
+            swiftSettings: [.enableExperimentalFeature("StrictConcurrency=complete")]
         ),
     ]
 )
